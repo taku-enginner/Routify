0539dac374bca14caa4de1ff9c4573b88cd97182
taku-enginner <taku.enginner@gmail.com>
2024-09-16 17:19:06 +0900

    deviseで認証完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :routines
-  # has_manyは1つのuserに対して複数のroutineが紐づけられることを意味する。その際、複数形を用いる。
-
-  validates :name, presence: true
-  validates :password, presence: true, length: { minimum: 5 }
 end
